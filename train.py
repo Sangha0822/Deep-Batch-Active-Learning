e4dbd47e2733bc6d3bd2f0346bee3b092d758f36
Sangha Jeon <sangha082214@gmail.com>
2025-08-12 16:06:24 -0700

    Successful implementation of RAND baseline

--- train.py
+++ train.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 import utils as utils
 import model as model
 
+import numpy as np
+from torch.utils.data import Subset
 
 
 def train(epochs, model, train_dataloader, test_dataloader, loss_fn, optimizer, device):
@@ -82,14 +84,21 @@ def train(epochs, model, train_dataloader, test_dataloader, loss_fn, optimizer, 
             print(f"Training accuracy reached 99.0% at epoch {epoch}. Stopping training for this model.")
             break 
 
+    return test_accuracy
+
 if __name__ == "__main__":
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 
-    
+    QUERY_ROUNDS = 10
+    BATCH_SIZE = 100
+
+    INITIAL_LABELS = 100
+
     transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.1307,), (0.3081,))
     ])
+
 
     train_dataset = datasets.MNIST(
         root = "./data",
@@ -106,33 +115,72 @@ if __name__ == "__main__":
     )
 
     train_dataloader = DataLoader(dataset= train_dataset, 
-                                batch_size= 64, 
+                                batch_size= BATCH_SIZE,
                                 shuffle=True
                                 )
     test_dataloader =  DataLoader(dataset= test_dataset, 
-                                batch_size= 64, 
+                                batch_size= BATCH_SIZE, 
                                 shuffle= True
                                 )
-
-
-    model_0 = model.ModelV0().to(device)
-
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(
-                                model_0.parameters(),
-                                lr = 0.001 
-                                )
     
-    train(
-        epochs=100, 
-        model=model_0,
-        train_dataloader=train_dataloader,
-        test_dataloader = test_dataloader,
-        loss_fn=loss_fn,
-        optimizer=optimizer,
-        device=device
-    )
+    num_train_data = len(train_dataset)
+    all_indices = list(range(num_train_data))
 
+    np.random.shuffle(all_indices)
+
+    labeled_indices = all_indices[:INITIAL_LABELS]
+    unlabeled_indices = all_indices[INITIAL_LABELS:]
+
+    print(f"--- Data Pool Initialization ---")
+    print(f"Initial labeled pool size: {len(labeled_indices)}")
+    print(f"Initial unlabeled pool size: {len(unlabeled_indices)}")
+    print("---------------------------------")
+
+    results_file = open("results_random.txt", "w")
+
+    for i in range(QUERY_ROUNDS):
+        model_0 = model.ModelV0().to(device)
+
+        loss_fn = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(
+                                    model_0.parameters(),
+                                    lr = 0.001 
+                                    )
+        
+        labeled_subset = Subset(train_dataset, labeled_indices)
+
+        active_train_dataloader = DataLoader(dataset=labeled_subset,
+                                             batch_size= BATCH_SIZE,
+                                             shuffle=True
+                                            )
+         
+        final_accuracy = train(
+            epochs=100, 
+            model=model_0,
+            train_dataloader= active_train_dataloader,
+            test_dataloader = test_dataloader,
+            loss_fn=loss_fn,
+            optimizer=optimizer,
+            device=device
+        )
+        
+        queries_indices = np.random.choice(unlabeled_indices,BATCH_SIZE, replace=False)
+        queried_set = set(queries_indices) # added set so it will remove duplicates faster
+        
+        # Below is logic to remove the new queried indices from the unlabled indices.
+        new_unlabled_indices = []
+        for index in unlabeled_indices:
+            if index not in queried_set:
+                new_unlabled_indices.append(index)
+        unlabeled_indices = new_unlabled_indices
+
+        labeled_indices.extend(queries_indices)
+
+        # Write the result into the results_random.txt file
+        log_entry = f"Round: {i+1}, Labeled Samples: {len(labeled_indices)}, Test Accuracy: {final_accuracy:.2f}%\n"
+        results_file.write(log_entry)
+        print(log_entry)
+    results_file.close()
 # Save and load:
 # save_model(model_0, target_dir="models", model_name="mnist_v0.pth")
 # model_loaded = utils.load_model("models/mnist_v0.pt", device=device)
